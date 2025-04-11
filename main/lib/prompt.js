32bce3a933eb82370e0d3f2194ced6f2440dff30
Emanuele Favero <emafaux89@gmail.com>
2025-04-11 21:14:04 +0200

    feat: add custom stylesheet to prompt package

--- main/lib/prompt.js
+++ main/lib/prompt.js
@@ -1,4 +1,5 @@
 import prompt from 'electron-prompt'
+import path from 'path'
 
 /**
  * Shows a prompt dialog with customizable title and input label
@@ -7,11 +8,16 @@ import prompt from 'electron-prompt'
  * @returns {Promise<string|null>} The user input or null if cancelled
  */
 export function showPrompt(title, value = '') {
+  // NOTE: Make sure that the CSS file is in the same directory as this file and check if it will still work after building
+  const __dirname = import.meta.dirname // ? added in Node.js v21.2.0, v20.11.0
+  const customStylesheet = path.join(__dirname, './prompt.css')
+
   return prompt({
     title,
+    value, // ? Useful for editing tasks
+    customStylesheet, // Path to the CSS file
     label: 'Task:',
     type: 'input',
-    value,
     buttonLabels: {
       ok: 'Done',
       cancel: 'Cancel',

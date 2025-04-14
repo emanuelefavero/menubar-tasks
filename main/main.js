251cdf653cd2d2ff5e489a556f7eb961ba59abff
Emanuele Favero <emafaux89@gmail.com>
2025-04-14 15:33:00 +0200

    docs: add TODO comment

--- main/main.js
+++ main/main.js
@@ -2,6 +2,7 @@ import { app } from 'electron'
 import { initializeTray } from './tray/tray.js'
 
 // TODO make sure to remove `resizable: true` from the prompt window
+// TODO avoid adding a task with an empty string
 // TODO add a mark task as done (by setting enabled: false for greyed out text and an icon to show done or strikethrough text if possible) when clicking on the task item
 // TODO add a settings menu item to open a settings window
 // TODO add max tasks setting to limit the number of tasks shown in the menu

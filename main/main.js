af78bf026f0765a9772427be404a930de0817111
Emanuele Favero <emafaux89@gmail.com>
2025-04-18 17:16:33 +0200

    chore: update TODO comments

--- main/main.js
+++ main/main.js
@@ -5,8 +5,8 @@ import { initializeTray } from './tray/tray.js'
 // TODO move "Add Task" option before the Tasks in the menu
 // TODO ? replace input with textarea to show more line of text in prompt (you should probably use a custom prompt since electron-prompt does not support it)
 // TODO add a max characters setting to limit the number of characters the user can input when adding or editing a task (135 chars is when the text is truncated)
-// TODO add a setting to choose if showing the last task, an icon, the app name "Tasks" in the tray icon
 // TODO create and add app icon
+// TODO add a setting to choose if showing the last task, an icon, the app name "Tasks" in the tray icon
 
 // Prevent the app from quitting when all windows are closed
 app.on('window-all-closed', (e) => {

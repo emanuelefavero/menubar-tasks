7afbcfe64f55608017841b1fb2e8bfc94a83f082
Emanuele Favero <emafaux89@gmail.com>
2025-04-21 20:16:11 +0200

    chore: remove TODO comments

--- main/main.js
+++ main/main.js
@@ -1,7 +1,6 @@
 import { app } from 'electron'
 import { initializeTray } from './tray/tray.js'
 
-// TODO add borders and different background color to the container class in globals.css which is used in settings.html (like the Mac OS settings menu)
 // TODO move "Add Task" option before the Tasks in the menu
 // TODO ? replace input with textarea to show more line of text in prompt (you should probably use a custom prompt since electron-prompt does not support it)
 // TODO add a max characters setting to limit the number of characters the user can input when adding or editing a task (135 chars is when the text is truncated)

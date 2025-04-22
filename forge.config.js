dd2555555d3b089fe3e4ebb7f6cf5fe3d35544b6
Emanuele Favero <emafaux89@gmail.com>
2025-04-22 06:52:53 +0200

    feat: remove unused forge makers and setup installer icon

--- forge.config.js
+++ forge.config.js
@@ -9,20 +9,13 @@ export default {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        options: {
+          icon: './assets/icon.png',
+        },
+      },
     },
   ],
   plugins: [

69632cf218ea9c9849f4fbb1b322ff6880d4fbe9
Emanuele Favero <emafaux89@gmail.com>
2025-04-24 02:08:57 +0200

    chore: update TODO comments

--- main/main.js
+++ main/main.js
@@ -1,8 +1,8 @@
 import { app } from 'electron'
 import { initializeTray } from './tray/tray.js'
 
-// TODO: add `titleBarStyle: 'hidden',` to prompt and settings BrowserWindow object while still allowing moving the window when clicking on the top of the window
 // TODO add a setting to choose if showing the last task, an icon, the app name "Tasks" in the tray icon
+// TODO add a setting to open the app on startup
 
 // Prevent the app from quitting when all windows are closed
 app.on('window-all-closed', (e) => {

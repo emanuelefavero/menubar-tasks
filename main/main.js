53f9a91cd5bef6310801b160c65b540f3631695b
Emanuele Favero <emafaux89@gmail.com>
2025-05-12 06:21:34 +0200

    chore: remove TODO comments

--- main/main.js
+++ main/main.js
@@ -3,8 +3,6 @@ import { initializeTray } from './tray/tray.js'
 import { loadSettings } from './services/settings.js'
 import AutoLaunch from 'auto-launch'
 
-// TODO Add "Clear Done Tasks" option to the tray menu (disable it if no tasks are done)
-// TODO Add keyboard shortcut accelerator to the "Clear Done Tasks" option, try CommandOrControl+Shift+D
 // TODO Try to add a tutorial to the app that will show the user how to use the app. The tutorial should be shown when the app is first installed, and should not be shown again after that unless the user clicks a "Show Tutorial" button in the "App" submenu. If the tutorial is ugly, remove it and follow next TODO 👇
 // TODO Add initial tasks (only if the tutorial is not added), a list of tasks to show when the app is first installed that will teach the user how to use the app. Make sure to only add these tasks when the app is first installed, and never again.
 // TODO Add multiple language support (English, German, French, Spanish, Italian, Portuguese, Russian, Chinese, Japanese, Korean)

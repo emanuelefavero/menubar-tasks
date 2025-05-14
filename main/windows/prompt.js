8c04acc925eced6bb7597d1c60eb017e3acb005b
Emanuele Favero <emafaux89@gmail.com>
2025-05-14 06:57:02 +0200

    feat: increase prompt window to fit new margins

--- main/windows/prompt.js
+++ main/windows/prompt.js
@@ -32,7 +32,7 @@ export function createPromptWindow(title = 'Add Task', value = '') {
     promptWindow = new BrowserWindow({
       titleBarStyle: 'hidden', // Hide the title bar
       width: 400,
-      height: 215,
+      height: 219,
       title,
       minimizable: false,
       maximizable: false,

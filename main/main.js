74e226346e8af1b1362c5c969135c9ec7304a489
Emanuele Favero <emafaux89@gmail.com>
2025-05-14 07:39:58 +0200

    chore: add TODO comments

--- main/main.js
+++ main/main.js
@@ -4,9 +4,12 @@ import { loadSettings } from './services/settings.js'
 import AutoLaunch from 'auto-launch'
 import { showTutorialIfFirstLaunch } from './windows/tutorial.js'
 
+// TODO focus on the app when the tutorial first launches so the user can see the tray icon and the app while reading the tutorial
+// TODO Add red circles to the tutorial images to indicate the steps
 // TODO When the user clicks on the tutorial step 1 image, automatically open and focus on the app from the tray icon
 // TODO Add a keyboard shortcut to focus on the app when the app is running
 // TODO Add a setting to change and enable the shortcut
+// TODO Add three tasks to show when the application is first installed
 // TODO Add multiple language support (English, German, French, Spanish, Italian, Portuguese, Russian, Chinese, Japanese, Korean)
 // TODO Add tests
 // TODO Add notarization with electron builder or npm electron-notarize @see https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/ and https://www.electron.build/code-signing-mac.html
